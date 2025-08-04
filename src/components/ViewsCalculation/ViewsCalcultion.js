b7ecae33e6e0963e09404a372eec520c0d2ce528
suyambu-raja <asuyamburaja35@gmail.com>
2025-08-04 15:15:46 +0530

    Update ViewsCalcultion.js

--- src/components/ViewsCalculation/ViewsCalcultion.js
+++ src/components/ViewsCalculation/ViewsCalcultion.js
@@ -8,17 +8,3 @@ export  const formatNumber = (num) => {
 
         return num.toString()
     }
-
-
-export function convertYouTubeDuration(isoDuration) {
-  const match = isoDuration.match(/PT(?:(\d+)H)?(?:(\d+)M)?(?:(\d+)S)?/);
-
-  const hours = parseInt(match[1] || 0, 10);
-  const minutes = parseInt(match[2] || 0, 10);
-  const seconds = parseInt(match[3] || 0, 10);
-
-  if (hours > 0) return `${hours} hr ${minutes} min`;
-  if (minutes > 0) return `${minutes} min`;
-  return `${seconds} sec`;
-}
-
